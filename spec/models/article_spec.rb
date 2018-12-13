34c8c7066f7f48b4a70276c634d43242f4d1b486
Sushil <sushilthe@gmail.com>
2018-12-14 03:22:57 +1100

    Test Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should test that the factory is valid' do
+      expect(build(:article)).to be_valid
+    end
+
+    it 'should validate the presence of the title' do
+      article = build :article, title: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the content' do
+      article = build :article, content: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of the slug' do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate the uniqueness of the slug' do
+      article = create :article
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+      expect(invalid_article.errors.messages[:slug]).to include('has already been taken')
+    end
+  end
 end
