4b63d090bd536e237ebe663c5b2ec46ecc411ac1
Sushil <sushilthe@gmail.com>
2018-12-14 18:06:10 +1100

    Add and test Article pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
-  def render(options={})
-    options[:json] = serializer.new(options[:json])
+  def render(options = {})
+    pagination = PaginationMetaGenerator
+                 .new(request: request,
+                      total_pages: options[:json].total_pages)
+                 .generate
+    options[:json] = serializer.new(options[:json], pagination)
     super(options)
   end
 end
